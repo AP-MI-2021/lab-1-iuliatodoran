0f098d653cb822a09ef3c91023b8a75c848e1f6a
Iulia Todoran <91527130+iuliatodoran@users.noreply.github.com>
2022-02-10 04:53:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,43 +1,65 @@
-'''
-Returneaza true daca n este prim si false daca nu.
-'''
-def is_prime(n):
-  if n<2:
+def is_prime(nr):
+  if nr < 2:
     return False
-  else:
-    for i in range(2,n//2+1):
-    if n%i==0:
+  for i in range(2,nr):
+    if nr % i ==0:
       return False
   return True
-  
-'''
-Returneaza produsul numerelor din lista lst.
-'''
-def get_product(lst):
-  p=1
-  for i in lst:
-  i=int(input("Dati numarul: "))
-      p=p*i
-  return p
-  
-'''
-Returneaza CMMDC a doua numere x si y folosind primul algoritm.
-'''
-def get_cmmdc_v1(x, y):
-  while x!=y:
-    if x>y: x-y
-    else y=y-x
-      print("cmmdc=",x)
-  
-'''
-Returneaza CMMDC a doua numere x si y folosind al doilea algoritm.
-'''
-def get_cmmdc_v2(x, y):
-  # codul vostru aici
-  
-  
+
+def get_prod(a,b,c):
+  return a*b*c
+
+def cmmdc1(n,m):
+  while n!=m:
+    if n>m:
+      n=n-m
+    else:
+      m=m-n
+
+  return n
+def cmmdc2(n,m):
+  while m != 0:
+    r=n%m
+    n=m
+    m=r
+  return n
+
+
+
 def main():
   # interfata de tip consola aici
+  f = False
+  while f != True:
+    print("1 Determinare nr prim")
+    print("2 Produsul a n nr naturale")
+    print("3 Prima metoda de aflare a CMMDC")
+    print("4 A doua metoda de aflare a CMMDC")
+    print("5. Iesire")
+    optiune = input("Alege obtiunea")
+    if optiune == "1":
+      nr=int(input("Dati un nr : "))
+      if is_prime(nr):
+        print("Nr este prim ")
+      else:
+        print("Nr nu este prim")
+    if optiune == "2":
+      nr1 = int(input("Nr 1 este:"))
+      nr2 = int(input("Nr 2 este:"))
+      nr3 = int(input("Nr 3 este:"))
+      p = get_prod(nr1,nr2,nr3)
+      print(f"Produsul dintre {nr1},{nr2},{nr3} este = {p}")
 
-if __name__ == '__main__':
-  main()
+    if optiune == "3":
+      n = int(input("Nr 1 este:"))
+      m = int(input("Nr 2 este:"))
+      cmd = cmmdc1(n,m)
+      print(f"Cmmdc a nr {n},{m} este {cmd}")
+
+    if optiune =="4":
+      n = int(input("Nr 1 este:"))
+      m = int(input("Nr 2 este:"))
+      cmd = cmmdc2(n,m)
+      print(f"Cmmdc a nr {n},{m} este {cmd}")
+
+    if optiune =="5":
+      f = True
